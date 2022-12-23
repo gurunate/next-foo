bc708b68cfe9500e72deb297fbf3be59ca2c3712
Nate Johnson <njohnson@peak6.com>
2022-12-23 11:31:13 -0600

    wip

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import { AppBar, Box, Container, Toolbar } from '@mui/material';
+import {
+    AppBar,
+    Box,
+    Button,
+    Collapse,
+    Container,
+    Toolbar
+} from '@mui/material';
+import { useScrollDirection } from 'use-scroll-direction';
 
 export type LayoutProps = {
     children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+    const { scrollDirection } = useScrollDirection();
+
+    const [collapse, setCollapse] = React.useState(false);
+
+    const toggle = () => {
+        setCollapse(prev => !prev);
+    };
+
+    React.useEffect(() => {
+        if (['DOWN', 'UP'].includes(scrollDirection))
+            setCollapse(scrollDirection === 'DOWN');
+    }, [scrollDirection]);
+
     return (
         <>
             <AppBar position="fixed">
-                <Toolbar>Heading</Toolbar>
+                <Collapse in={!collapse}>
+                    <Toolbar>Search</Toolbar>
+                </Collapse>
+                <Toolbar sx={{ backgroundColor: 'grey' }}>Nav</Toolbar>
             </AppBar>
             <Container>
-                <Box mt={10}>{children}</Box>
+                <Box mt={20}>{children}</Box>
             </Container>
         </>
     );
