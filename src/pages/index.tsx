3059f528e78ccaff9aed8e1355a5bdfa3fdb2732
Nate Johnson <njohnson@peak6.com>
2022-12-23 09:29:26 -0600

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+import { faker } from '@faker-js/faker';
 
 import Layout from '@/components/layout';
 
 const Home: NextPage = () => {
-    return <Layout>coming...</Layout>;
+    return (
+        <Layout>
+            {faker.lorem
+                .paragraphs(100)
+                .split(/\n/)
+                .map((p, idx) => (
+                    <Typography key={idx} paragraph>
+                        {p}
+                    </Typography>
+                ))}
+        </Layout>
+    );
 };
 
 export default Home;
