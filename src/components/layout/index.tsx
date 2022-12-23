194cbd232d74964d735d0666074ec9237decb670
Nate Johnson <njohnson@peak6.com>
2022-12-23 09:42:29 -0600

    wip

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { AppBar, Box, Container, Toolbar } from '@mui/material';
 
 export type LayoutProps = {
     children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-    return <Container>{children}</Container>;
+    return (
+        <>
+            <AppBar position="fixed">
+                <Toolbar>Heading</Toolbar>
+            </AppBar>
+            <Container>
+                <Box mt={10}>{children}</Box>
+            </Container>
+        </>
+    );
 };
 
 export default Layout;
