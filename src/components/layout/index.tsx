e1dae887d7834ba4d2cc13ad8a920b1abd640db5
Nate Johnson <njohnson@peak6.com>
2022-12-23 11:40:34 -0600

    wip

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import {
-    AppBar,
-    Box,
-    Button,
-    Collapse,
-    Container,
-    Toolbar
-} from '@mui/material';
+import { AppBar, Box, Collapse, Container, Toolbar } from '@mui/material';
 import { useScrollDirection } from 'use-scroll-direction';
 
 export type LayoutProps = {
     children: React.ReactNode;
 };
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const SCROLL_DOWN = 'DOWN';
+const SCROLL_UP = 'UP';
+
+/**
+ * Layout component
+ *
+ * @param {LayoutProps} props
+ * @returns {React.ReactElement}
+ */
+const Layout: React.FC<LayoutProps> = ({
+    children
+}: LayoutProps): React.ReactElement => {
     const { scrollDirection } = useScrollDirection();
 
     const [collapse, setCollapse] = React.useState(false);
 
-    const toggle = () => {
-        setCollapse(prev => !prev);
-    };
-
     React.useEffect(() => {
-        if (['DOWN', 'UP'].includes(scrollDirection))
-            setCollapse(scrollDirection === 'DOWN');
+        if ([SCROLL_DOWN, SCROLL_UP].includes(scrollDirection))
+            setCollapse(scrollDirection === SCROLL_DOWN);
     }, [scrollDirection]);
 
     return (
